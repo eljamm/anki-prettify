d621aa0d9dc27002c5a4580075d49d844f54ec46
eljamm <fedi.jamoussi@protonmail.ch>
2024-11-10 15:21:17 +0100

    feat(nix): init development environment

---
+++ devenv.nix
@@ -0,0 +1,60 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}:
+
+{
+  # https://devenv.sh/packages/
+  packages = [ pkgs.dart-sass ];
+
+  cachix.enable = false;
+
+  # https://devenv.sh/languages/
+  # languages.rust.enable = true;
+  languages.python = {
+    enable = true;
+    venv.enable = true;
+    venv.requirements = ''
+      cached-property==1.5.2
+      certifi==2022.12.7
+      charset-normalizer==3.0.1
+      chevron==0.14.0
+      frozendict==2.3.4
+      genanki==0.13.1
+      idna==3.4
+      requests==2.28.2
+      urllib3==1.26.14
+    '';
+    libraries = [ pkgs.python3Packages.pyyaml ];
+  };
+
+  # https://devenv.sh/processes/
+  processes.sass-watch.exec = "sass --no-source-map src/styles/scss:src/styles/css --watch";
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/scripts/
+  scripts.sb.exec = "sass --no-source-map src/styles/scss:src/styles/css";
+  scripts.sw.exec = "sass --no-source-map src/styles/scss:src/styles/css --watch";
+
+  # https://devenv.sh/tasks/
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+  # # https://devenv.sh/tests/
+  # enterTest = ''
+  #   echo "Running tests"
+  #   git --version | grep --color=auto "${pkgs.git.version}"
+  # '';
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
